aec85cdb4c17954e525970d6b9de517f83864e51
RezoanulHasan <rezoanulhasan96@gmail.com>
2024-07-03 07:11:15 +0600

    work done

--- src/app.ts
+++ src/app.ts
@@ -6,17 +6,17 @@ import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import notFound from './app/middlewares/notFound';
 
-import cookieParser from 'cookie-parser';
 import globalErrorMainHandler from './app/middlewares/globalErrorMainHandler';
 
 import router from './app/routes';
 const app: Application = express();
 
-//parsers
 app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({ origin: 'https://taskshubrezoanul.netlify.app', credentials: true }),
+);
 //app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
 //routes
 app.use('/api', router);
 
